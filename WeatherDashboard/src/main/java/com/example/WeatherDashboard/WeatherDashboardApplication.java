20958e66f2607af13b32239f6bab91a7a486c874
lindsayowow <lindsayow@gmail.com>
2026-08-05 00:19:44 -0500

    working on main method, updated dependencies

--- WeatherDashboard/src/main/java/com/example/WeatherDashboard/WeatherDashboardApplication.java
+++ WeatherDashboard/src/main/java/com/example/WeatherDashboard/WeatherDashboardApplication.java
@@ -2,12 +2,25 @@ package com.example.WeatherDashboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import com.example.WeatherDashboard.service.WeatherService;
+import com.example.WeatherDashboard.model.WeatherInfo;
+
 
 @SpringBootApplication
 public class WeatherDashboardApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WeatherDashboardApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner testWeather(WeatherService weatherService) {
+		return args -> {
+			WeatherInfo info = weatherService.getWeatherInfo("London");
+			System.out.println(info);
+		};
 	}
 
 }
