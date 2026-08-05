8bc619075138d845cab013c4d5c50a6ae9923135
lindsayowow <lindsayow@gmail.com>
2026-08-04 22:15:18 -0500

    Adding Maven structural files

---
+++ WeatherDashboard/src/test/java/com/example/WeatherDashboard/WeatherDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.WeatherDashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
