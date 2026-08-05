13b15f9f2df8cfe9b3ca1ad0fd98f1b3b504b6cc
lindsayowow <lindsayow@gmail.com>
2026-08-05 00:36:52 -0500

    updating syntax

--- WeatherDashboard/src/main/java/com/example/WeatherDashboard/config/WeatherAPIConfig.java
+++ WeatherDashboard/src/main/java/com/example/WeatherDashboard/config/WeatherAPIConfig.java
@@ -6,10 +6,11 @@ import org.springframework.stereotype.Component;
 @Component
 public class WeatherAPIConfig {
 
-    @Value("${weather.api.key}")
+    @Value("${WEATHER_API_KEY}")
     private String apiKey;
 
     public String getApiKey() {
         return apiKey;
     }
 }
+
